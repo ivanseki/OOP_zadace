a92167706ff76f04c9866973e2d2b0626463f5ad
ivanseki <ivan.chernobyl007@gmail.com>
2021-11-29 21:19:42 +0100

    Preparing for second homework

--- DZ1/Windchill/Program.cs
+++ DZ1/Windchill/Program.cs
@@ -6,7 +6,8 @@ namespace Windchill
     {
         static void Main(string[] args)
         {
-            RunDemoForHW1();
+            //RunDemoForHW1();
+            RunDemoForHW2();
         }
         
         private static void RunDemoForHW1()
@@ -60,6 +61,37 @@ namespace Windchill
             }
 
             return WeatherWithLargestWindchill;
+        }
+        
+        private static void RunDemoForHW2()
+        {
+            DateTime monday = new DateTime(2021, 11, 8);
+            Weather mondayWeather = new Weather(6.17, 56.13, 4.9);
+            DailyForecast mondayForecast = new DailyForecast(monday, mondayWeather);
+            Console.WriteLine(monday.ToString());
+            Console.WriteLine(mondayWeather.GetAsString());
+            Console.WriteLine(mondayForecast.GetAsString());
+
+            // Assume a valid input file (correct format).
+            // Assume that the number of rows in the text file is always 7. 
+            string fileName = "weather.forecast";
+            if (File.Exists(fileName) == false)
+            {
+                Console.WriteLine("The required file does not exist. Please create it, or change the path.");
+                return;
+            }
+
+            string[] dailyWeatherInputs = File.ReadAllLines(fileName);
+            DailyForecast[] dailyForecasts = new DailyForecast[dailyWeatherInputs.Length];
+            for (int i = 0; i < dailyForecasts.Length; i++)
+            {
+                dailyForecasts[i] = ForecastUtilities.Parse(dailyWeatherInputs[i]);
+            }
+            WeeklyForecast weeklyForecast = new WeeklyForecast(dailyForecasts);
+            Console.WriteLine(weeklyForecast.GetAsString());
+            Console.WriteLine("Maximal weekly temperature:");
+            Console.WriteLine(weeklyForecast.GetMaxTemperature());
+            Console.WriteLine(weeklyForecast[0].GetAsString());
         }
     }
 }
